a47e24e22dca52338139161050f66e5ec84b2e4e
pkhermouch <844064+pkhermouch@users.noreply.github.com>
2023-12-07 21:49:07 -0600

    Instructions in plugin-hub readme

--- src/main/java/com/examineplayer/ExaminePlayerPlugin.java
+++ src/main/java/com/examineplayer/ExaminePlayerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.examineplayer;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Examine Player"
 )
-public class ExamplePlugin extends Plugin
+public class ExaminePlayerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ExaminePlayerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ExaminePlayerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ExaminePlayerConfig.class);
 	}
 }
