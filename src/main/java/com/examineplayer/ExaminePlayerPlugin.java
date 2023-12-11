e7085bf18a465b150016acd4a42dcf19cf2e9cb2
Paul K <844064+pkhermouch@users.noreply.github.com>
2023-12-10 19:00:20 -0600

    Added methods for communicating with Runelite API

--- src/main/java/com/examineplayer/ExaminePlayerPlugin.java
+++ src/main/java/com/examineplayer/ExaminePlayerPlugin.java
@@ -1,17 +1,22 @@
 package com.examineplayer;
 
 import com.google.inject.Provides;
-import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.chat.ChatClient;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.http.api.chat.Task;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Slf4j
 @PluginDescriptor(
@@ -19,11 +24,18 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class ExaminePlayerPlugin extends Plugin
 {
+	private final String REDIS_KEY_PREFIX = "examineplayer:";
 	@Inject
 	private Client client;
 
 	@Inject
 	private ExaminePlayerConfig config;
+
+	@Inject
+	private ChatClient chatClient;
+
+	@Inject
+	private ScheduledExecutorService executor;
 
 	@Override
 	protected void startUp() throws Exception
@@ -57,6 +69,46 @@ public class ExaminePlayerPlugin extends Plugin
 		{
 			log.info(String.format("Config changed! Examine text: %s", config.getExamineText()));
 		}
+	}
+
+	private void setExamineText() {
+		// Since we don't have a generic API endpoint to store an arbitrary string in the Redis store so that other
+		// clients can read information about this player, piggy-back off of the Slayer plugin's endpoint to store
+		// slayer information.
+		// Add a prefix to the player name so that our Redis keys are namespaced and won't ever collide with data
+		// written by the Slayer plugin.
+		final String playerName = client.getLocalPlayer().getName();
+		final String examineText = config.getExamineText().substring(0, 140);
+		executor.execute(() ->
+		{
+			try
+			{
+				chatClient.submitTask(
+						String.format("%s%s", REDIS_KEY_PREFIX, playerName),
+						examineText,
+						50,
+						100,
+						"Atlantis");
+			}
+			catch (Exception ex)
+			{
+				log.warn("Unable to set examine text", ex);
+			}
+		});
+	}
+
+	private String getExamineText(String playerName) {
+		final String keyName = String.format("%s%s", REDIS_KEY_PREFIX, playerName);
+		Task task;
+		try {
+			task = chatClient.getTask(keyName);
+		}
+		catch (IOException e) {
+			log.warn(String.format("Unable to get examine text for player %s", playerName), e);
+			return "";
+		}
+
+		return task.getTask();
 	}
 
 	@Provides
